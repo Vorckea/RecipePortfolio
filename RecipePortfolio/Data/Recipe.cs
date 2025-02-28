223d4f7cdd7242538b472962b82c618e5094823e
Aksel Heggland Schrader <akselschrader@gmail.com>
2025-02-28 16:25:05 +0000

    Test

---
+++ RecipePortfolio/Data/Recipe.cs
@@ -0,0 +1,14 @@
+﻿namespace RecipePortfolio.Data
+{
+    public class Recipe
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public List<string> Ingredients { get; set; } = new();
+        public List<string> Instructions { get; set; } = new();
+        public string PrepTime { get; set; } = "";
+        public string Image { get; set; } = "";
+        public List<string> Tags { get; set; } = new();
+    }
+}
