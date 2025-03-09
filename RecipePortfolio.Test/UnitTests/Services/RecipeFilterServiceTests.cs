c82b05546631b1ac6604b6b1a839e1572be46022
Aksel Heggland Schrader <akselschrader@gmail.com>
2025-03-09 16:49:20 +0000

    Remove unused usings; add CodeCoverage component
    Removed unused using directives in RecipeFilterServiceTests.cs and RecipeServiceTests.cs. Made minor formatting changes in RecipeServiceTests.cs. Added new Razor component CodeCoverage.razor to display code coverage report.

--- RecipePortfolio.Test/UnitTests/Services/RecipeFilterServiceTests.cs
+++ RecipePortfolio.Test/UnitTests/Services/RecipeFilterServiceTests.cs
@@ -1,10 +1,5 @@
 ﻿using RecipePortfolio.Models;
 using RecipePortfolio.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RecipePortfolio.Test.UnitTests.Services
 {
