3e767adf3e9223153ccc53ad0e5647e3a2509d6f
Aksel Heggland Schrader <akselschrader@gmail.com>
2025-03-10 20:38:42 +0000

    Add XML documentation to IRecipeService and ITagService
    Added XML documentation comments to IRecipeService and ITagService interfaces. These comments provide detailed descriptions of the interfaces and their methods, including summaries, parameters, and return values, enhancing code readability and maintainability.

--- RecipePortfolio/Services/IRecipeService.cs
+++ RecipePortfolio/Services/IRecipeService.cs
@@ -2,9 +2,22 @@
 
 namespace RecipePortfolio.Services
 {
+    /// <summary>
+    /// A service for managing recipes.
+    /// </summary>
     public interface IRecipeService
     {
+        /// <summary>
+        /// Gets a list of recipes asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of <see cref="Recipe"/> objects.</returns>
         Task<List<Recipe>> GetRecipesAsync();
+
+        /// <summary>
+        /// Gets a recipe by its ID asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the recipe.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the <see cref="Recipe"/> object.</returns>
         Task<Recipe?> GetRecipeByIdAsync(string id);
     }
 }
--- RecipePortfolio/Services/ITagService.cs
+++ RecipePortfolio/Services/ITagService.cs
@@ -2,9 +2,24 @@
 
 namespace RecipePortfolio.Services
 {
+    /// <summary>
+    /// Service for managing tags.
+    /// </summary>
     public interface ITagService
     {
+        /// <summary>
+        /// Filters a list of tags based on a search term.
+        /// </summary>
+        /// <param name="tags">The list of tags to filter.</param>
+        /// <param name="searchTerm">The search term to filter tags by name</param>
+        /// <returns>A list of tags that match the search term.</returns>
         List<Tag> FilterTags(List<Tag> tags, string searchTerm);
+
+        /// <summary>
+        /// Gets tags from a list of recipes.
+        /// </summary>
+        /// <param name="recipes">The list of recipes to extract tags from.</param>
+        /// <returns>A list of tags extracted from the recipes.</returns>
         List<Tag> GetTagsFromRecipes(List<Recipe> recipes);
     }
 }
